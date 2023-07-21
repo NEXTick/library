699c6740d7eaf971b20b696ad25ea1893b4bb064
NEXTick <kirillms29@gmail.com>
2023-07-21 15:24:01 +0300

    custom login page in process

---
+++ src/main/java/com/example/library/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/start", true)
+                        .failureUrl("/auth/login?error")
+                        .permitAll());
+        return http.build();
+    }
+
+}
