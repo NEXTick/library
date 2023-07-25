dd05f19cd2c28bac1f552c1f618757c8dc3e5d3a
NEXTick <kirillms29@gmail.com>
2023-07-25 13:10:20 +0300

    csrf enable

--- src/main/java/com/example/library/config/SecurityConfig.java
+++ src/main/java/com/example/library/config/SecurityConfig.java
@@ -19,9 +19,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
+        http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
